6ddc1e77024b4657f2c32fbe0bc3f750d81f5402
aparna199817 <makkenaaparna17@gmail.com>
2020-12-14 21:21:34 +0530

    adding code

--- index.js
+++ index.js
@@ -86,3 +86,11 @@ console.log(userss.name);
 let lengths = ["Bilbo", "Gandalf", "Nazgul"].map(item => item.length);
 console.log(lengths);
 */
+//map concept
+let map=new Map();
+map.set('1','str1');
+map.set(1,'num1');
+map.set(true,'bool1');
+console.log(map.get(1));
+console.log(map.get('1'));
+console.log(map.size);
